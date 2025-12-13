4ffb1ad0981f675bce1dfba503a0ba46beead227
G Ranga Lalitha Jahnavi <jahnaviguturi1@gmail.com>
2025-12-13 13:03:39 +0530

    Time: 8 ms (82.27%), Space: 47.2 MB (61.82%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        HashSet<String> busCat = new HashSet<>();
+        Collections.addAll(busCat, "electronics", "grocery", "pharmacy", "restaurant");
+        List<String[]> temp = new ArrayList<>();// [bisCat, code]
+
+        for(int i=0; i<code.length; i++) {
+            if(!isActive[i] || code[i].isEmpty())
+                continue;
+            if(isAlphanumeric(code[i]) && busCat.contains(businessLine[i]) && isActive[i]) {
+                temp.add(new String[]{businessLine[i], code[i]});
+            }
+        }
+
+        Collections.sort(temp, (a,b)->{
+            if(!a[0].equals(b[0])) {
+                return a[0].compareTo(b[0]);
+            } else {
+                return a[1].compareTo(b[1]);
+            }
+        });
+
+        List<String> ans = new ArrayList<>();
+
+        for(String[] item : temp) {
+            ans.add(item[1]);
+        }
+        return ans;
+    }
+
+    boolean isAlphanumeric(String str) {
+        for(char ch : str.toCharArray()) {
+            if(Character.isDigit(ch) || Character.isLetter(ch) || ch=='_')
+                continue;
+            return false;
+        }
+        return str.length() > 0;
+    }
+}
