e00c10e513cc42cb604a982607997b91ac12c065
G Ranga Lalitha Jahnavi <jahnaviguturi1@gmail.com>
2025-12-20 12:50:45 +0530

    Time: 8 ms (74.4%), Space: 46.7 MB (72.32%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,18 +1,12 @@
 class Solution {
-    public boolean isSortedCol(int col, String[] strs) {
-        for(int i=1; i<strs.length; i++) {
-            if(strs[i].charAt(col) < strs[i-1].charAt(col)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public int minDeletionSize(String[] strs) {
         int cnt = 0;
         for(int i=0; i<strs[0].length(); i++) {
-            if(!isSortedCol(i, strs)) {
-                cnt++;
+            for(int j=1; j<strs.length; j++) {
+                if(strs[j].charAt(i) < strs[j-1].charAt(i)) {
+                    cnt++;
+                    break;
+                }
             }
         }
         return cnt;
