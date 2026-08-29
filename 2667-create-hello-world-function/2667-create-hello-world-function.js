ce821f37cb0ae76247d14f8dd68e2d100a59c371
G Ranga Lalitha Jahnavi <jahnaviguturi1@gmail.com>
2026-08-29 11:11:37 +0530

    Time: 52 ms (8.5%), Space: 54.1 MB (10.32%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    const greet = "Hello World";
+
+    return function(...args) {
+        return greet;
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
